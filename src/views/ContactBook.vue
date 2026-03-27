290aa1eea1c318cc78a878391293902473ea29dc
truongduy-code <duynguyen.280905@gmail.com>
2026-03-28 01:21:55 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -36,11 +36,20 @@ Chi tiết Liên hệ
 <i class="fas fa-address-card"></i>
 </h4>
 <ContactCard :contact="activeContact" />
+<router-link
+    :to="{
+    name: 'contact.edit',
+    params: { id: activeContact._id },
+    }"
+    >
+    <span class="mt-2 badge badge-warning">
+    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+    >
+</router-link>
 </div>
 </div>
 </div>
 </template>
-
 <script>
 import ContactCard from "@/components/ContactCard.vue";
 import InputSearch from "@/components/InputSearch.vue";
@@ -120,7 +129,6 @@ this.refreshList();
 },
 };
 </script>
-
 <style scoped>
 .page {
 text-align: left;
