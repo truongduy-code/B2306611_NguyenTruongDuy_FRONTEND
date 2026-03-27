d563595708f17748d6bbb1a09ebf40e626af9698
truongduy-code <duynguyen.280905@gmail.com>
2026-03-28 01:37:41 +0700

    Tao trang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,12 @@ const routes = [
         component: () => import("@/views/ContactEdit.vue"),
         props: true
     },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true
+    },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
